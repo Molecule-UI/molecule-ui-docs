92c3e1e804a9060d413bb2f9ae119fdc89845d7a
rushildhinoja <rushil.dhinoja@byjus.com>
2022-11-13 14:38:52 +0530

    backup

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Default from "../components/Layout/Default"
 import StylesLayout from "../components/Styles/StylesLayout"
-import { navigate } from "gatsby"
 const styles = () => {
   return <Default></Default>
 }
